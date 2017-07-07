5f3c752809db91fbd499c95659f9dccb40730e05
ave2me <ave2@me.com>
2017-07-08 01:54:01 +0300

    run app remove

--- hello.py
+++ hello.py
@@ -1,20 +1,21 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.run('0.0.0.0')
-
 
 @app.route('/')
 def index():
     return 'Here will be an index page'
 
+
 @app.route('/hello')
 def hello():
     return 'Hello, World!'
 
+
 @app.route('/hello/<username>')
 def greet_user(username):
     return 'Hello, {}!'.format(username)
+
 
 @app.route('/ispalindrome/<string:word>')
 def is_palindrome(word):
